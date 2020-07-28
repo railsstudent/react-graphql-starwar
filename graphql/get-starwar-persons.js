d2fbfc9730ef87097c13937e6d52e2dd27b2060b
railsstudent <cleung334@gmail.com>
2020-07-28 11:13:45 +0800

    wip: client-side graphql

---
+++ graphql/get-starwar-persons.js
@@ -0,0 +1,28 @@
+// query {
+//   persons(
+// 	  where: { 
+//       name_contains: "ray"
+//   	},
+//   	first: 5,
+// 	) {
+//     id,
+//     name,
+//     height,
+//     birthYear,
+//     mass,
+//     skinColor,
+//     gender,
+//     eyeColor, 
+//     hairColor,
+//     homeworld {
+//       id,
+//       name,
+//       terrain,
+//     },
+//     species(first: 1) {
+//      id,
+//       name,
+//       language
+//     }
+// 	}
+// }
