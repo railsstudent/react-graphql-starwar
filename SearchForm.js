cd7d6b1f71de5c821a0fa608bd5c077cef49430d
railsstudent <cleung334@gmail.com>
2020-07-28 08:35:33 +0800

    wip: styling input field

--- SearchForm.js
+++ SearchForm.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 const SearchForm = () => {
   return (
-    <form class="form" onSubmit={() => console.log('submit clicked')}>
-      <label htmlFor="query" class="query-input">
-        Search:
-        <input id="query" />
+    <form className="form" onSubmit={() => console.log('submit clicked')}>
+      <label htmlForm="query" className="query-input">
+        <span className="query-text">Character:</span>
+        <input id="query" placeholder="i.e. Luke Skywalker" />
       </label>
-      <button type="submit" class="submit">Submit</button>
+      <button type="submit" className="submit">Submit</button>
     </form>
   )
 };
