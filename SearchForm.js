3ee97591174e5d4cc54f82d8614eac02908f2828
railsstudent <cleung334@gmail.com>
2020-07-28 10:31:23 +0800

    Create onSubmit handler

--- SearchForm.js
+++ SearchForm.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SearchForm = () => {
+  const [query, setQuery] = useState('');
+
+  const searchCharacters = (e) => {
+    e.preventDefault();
+    console.log(query)
+  }
+
   return (
-    <form className="form" onSubmit={() => console.log('submit clicked')}>
+    <form className="form" onSubmit={searchCharacters}>
       <label htmlForm="query" className="query-label">
         <span className="query-text">Character:</span>
-        <input id="query" className="query-input" placeholder="i.e. Luke Skywalker" />
+        <input 
+          id="query" 
+          type="input"
+          className="query-input"
+          placeholder="i.e. Luke Skywalker"
+          value={query}
+          onChange={ e => setQuery(e.target.value) }
+        />
       </label>
       <button type="submit" className="submit">Submit</button>
     </form>
