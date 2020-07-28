c541155fc2685270811bd8357864fe8576c94df7
railsstudent <cleung334@gmail.com>
2020-07-28 10:24:28 +0800

    style form

--- SearchForm.js
+++ SearchForm.js
@@ -3,9 +3,9 @@ import React from 'react';
 const SearchForm = () => {
   return (
     <form className="form" onSubmit={() => console.log('submit clicked')}>
-      <label htmlForm="query" className="query-input">
+      <label htmlForm="query" className="query-label">
         <span className="query-text">Character:</span>
-        <input id="query" placeholder="i.e. Luke Skywalker" />
+        <input id="query" className="query-input" placeholder="i.e. Luke Skywalker" />
       </label>
       <button type="submit" className="submit">Submit</button>
     </form>
