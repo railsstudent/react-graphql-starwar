d33db690d7f2b3581756ec77294074ec1e0a3bdd
railsstudent <cleung334@gmail.com>
2020-07-28 23:31:09 +0800

    style PersonCard

--- PersonCard.js
+++ PersonCard.js
@@ -2,6 +2,22 @@ import React from 'react';
 
 const PersonCard = (prop) => {
   const { person } = prop;
+  const { 
+    eyeColor: eyeColors = [], 
+    hairColor: hairColors = [], 
+    skinColor: skinColors = [], 
+    homeworld,
+    species
+  } = person || {}
+  const eyeColor = eyeColors.length > 0 && eyeColors[0] || undefined
+  const hairColor = hairColors.length > 0 && hairColors[0] || undefined
+  const skinColor = skinColors.length > 0 && skinColors[0] || undefined
+
+  console.log('homeworld', homeworld)
+  const { name = '' } = homeworld || {};
+  const [{ name: speciesName, language }] = species || [ { name: '', language: '' }];
+  console.log('speciesName', speciesName, language);
+
   return (
     <div className="person-container">
       <div className="name">
@@ -13,35 +29,39 @@ const PersonCard = (prop) => {
         </div>
         <div className="physical-measurement">
           <p className="title">Measurements</p>
-          <span>Mass: { person.mass }</span>
-          <span>Height: { person.height }</span>
-        </div>
-        <div className="physical-features">
-          <p className="title">Facial features</p>
-          <div className="eye-colors">
-            <span>Eye color: </span>
-            { person.eyeColor.map(color => <span className="eye-color" key={color}>{color}</span>)}
-          </div>
-          <div className="skin-colors">
-            <span>Skin color: </span>
-            { person.skinColor.map(color => <span className="skin-color" key={color}>{color}</span>)}
-          </div>
-          <div className="hair-colors">
-            <span>Hair color: </span>
-            { person.hairColor.map(color => <span className="hair-color" key={color}>{color}</span>)}
-          </div>
-        </div>
-        <div className="home-world">
-          <p className="title">Home world</p>
-          <p className="name">{ person.homeworld.name}</p>
-        </div>
-        <div className="species">
-          <p className="title">Species</p>
           <div>
-              <span>Name: </span><span>{ person.species[0].name}</span>
-              <span>Language: </span><span>{ person.species[0].language}</span>
+            <span>Mass: { person.mass || 'N/A' }</span>
+            <span>Height: { person.height || 'N/A' }</span>
           </div>
         </div>
+        <div className="facial-features">
+          <p className="title">Facial features</p>
+          <div className="colors">
+            { eyeColor && <div className="color">
+              <span>Eye color: </span>
+              <span className="eye-color" key={eyeColor}>{eyeColor}</span>
+            </div> }
+            { skinColor && <div className="color">
+              <span>Skin color: </span>
+              <span className="skin-color" key={skinColor}>{skinColor}</span>
+            </div> }
+            { hairColor && <div className="color">
+              <span>Hair color: </span>
+              <span className="hair-color" key={hairColor}>{hairColor}</span>
+            </div>}
+          </div>
+        </div>
+        { name && <div className="home-world">
+          <p className="title">Home world</p>
+          <p className="name">{ name}</p>
+        </div>}
+        { (speciesName || language) && <div className="species-container">
+          <p className="title">Species</p>
+          <div className="species">
+            { speciesName && <span>{ speciesName }</span> }
+            { language && <span>{ language}</span> }
+          </div>
+        </div>}
       </div>
     </div>
   )
