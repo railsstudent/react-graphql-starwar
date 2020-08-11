b367f5104858e080519b10ce43e296437a0d8dff
railsstudent <cleung334@gmail.com>
2020-08-12 00:43:56 +0800

    fix:  delete non-existing import

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-import { client, ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
   uri: "https://api-euwest.graphcms.com/v1/ck5wca13qc9ux01fgaidt12m4/master",
